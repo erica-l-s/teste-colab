4132eedcda944f7708ead34ece60d3be560597ea
erica-l-s <erica.l.s1996@gmail.com>
2023-04-12 18:52:49 -0300

    puxando as informaçoes 1 por um e fazer navlink

--- src/User/user.js
+++ src/User/user.js
@@ -6,21 +6,32 @@ const User = ()=>{
     const [userList, setUserList] = useState([])
     
 
-    useEffect(()=>{
-        axios.get('https://randomuser.me/api/?results=20')
+   const user = ()=>{
+        axios.get('https://randomuser.me/api/')
          .then((response)=>{
             setUserList(response.data.results)
          })
+    }
+
+    const onClickUser = () =>{
+        user()
+        setUserList()
+    }
+
+    useEffect(()=>{
+        onClickUser()
     },[])
 
-    console.log(userList)
-      return(
+     return(
         <div>
+           
+            
                 {userList && userList.map((list,index) =>{
             return(
               
                 <div key={index}>
                     <img src={list.picture.large}></img>
+                    <button value={userList} onClick={onClickUser}>New</button>
                     <h3>{list.name.title}. {list.name.first} {list.name.last}</h3>
                     <p>Age: {list.dob.age} years old</p>
                     <p>City: {list.location.city}, {list.location.country}</p>
